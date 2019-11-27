44f21e5caf3981a1ea51b82fd24cd68fe1f08483
chenbo <18451542@qq.com>
2019-11-27 15:16:02 +0800

    no message

--- RegexDefine.cs
+++ RegexDefine.cs
@@ -8,15 +8,15 @@ namespace ReadWordForms
 {
     class RegexDefine
     {
-        public static Regex containsIntOrfloat = new Regex(@"\d+([.]{1}\d+){0,1}");
-        public static Regex isIntOrfloat = new Regex(@"^\d+([.]{1}\d+){0,1}$");
+        public static Regex containsIntOrfloat = new Regex(@"\d+([.]\d+)?");
+        public static Regex isIntOrfloat = new Regex(@"^\d+([.]\d+){0,1}$");
         public static Regex isFloat = new Regex(@"^\d+[.]\d+$");
         public static Regex isCh = new Regex(@"[\u4e00 - \u9fa5]");
         public static Regex containsGe = new Regex(@"\d+(?=\u4e2a)");//*个
         public static Regex containsYuan = new Regex(@"\d+(?=\u5143)");//*元
         public static Regex containsPerge = new Regex(@"\d+(?=/\u4e2a)");//*/个
-        public static Regex containsEqualInLeft = new Regex(@"(?<==)\d+[.]?\d+?");//=*
-        public static Regex containsEqualInRight = new Regex(@"\d+?[.]?\d+(?==)");//*=
+        public static Regex containsEqualInLeft = new Regex(@"(?<==)\d+([.]\d+)?");//=*
+        public static Regex containsEqualInRight = new Regex(@"(\d+[.])?\d+(?==)");//*=
         public static Regex isDate = new Regex(@"(10|11|12|[0]?\d)[.]\s{0,2}[0-3]?\d");//12.30,1.05,1.5,01.5
 
         public static Regex containsWidth = new Regex(@"[\u5bbd]\d+");//宽*
